c795d80ae68f1b8e93f00e45f8e5ee0cf0e8d1c0
ANUPAM KHOSLA <anupam.khosla@oxideinteractive.com.au>
2022-10-28 17:36:28 +1100

    Added backup servers logic, reorganized functions for url query

--- src/js/App.js
+++ src/js/App.js
@@ -20,37 +20,66 @@ class App extends React.Component {
       loading: true, //this is changed to trigger re render after fetch loading is complete      
       error: false,   // in case fetch gives 404 etc, change this to trigger re render with error msg        
     };    
-
-    
-    this.FreshSearch(); //execute brand new search based upon url params    
+    this.resetQuery();
+    this.selectedServer = undefined;
+    this.servers = [
+                    {
+                        "ip": "95.179.139.106",
+                        "name": "nl1.api.radio-browser.info"
+                    },
+                    {
+                        "ip": "89.58.16.19",
+                        "name": "at1.api.radio-browser.info"
+                    },
+                    {
+                        "ip": "188.68.62.16",
+                        "name": "de1.api.radio-browser.info"
+                    },
+                    {
+                        "ip": "2a03:4000:6:8077::1",
+                        "name": "de1.api.radio-browser.info"
+                    },
+                    {
+                        "ip": "2a0a:4cc0:0:db9:282b:91ff:fed0:ddea",
+                        "name": "at1.api.radio-browser.info"
+                    },
+                    {
+                        "ip": "2001:19f0:5001:32a4:5400:2ff:fe37:75c2",
+                        "name": "nl1.api.radio-browser.info"
+                    }
+                ]    
+    this.selectServer(0); //if nth server fails it will try n+1th server
+    //also will call FreshSearch() on success
 
     this.onTriggerSearch = this.onTriggerSearch.bind(this);
     this.onToggle = this.onToggle.bind(this);
     this.onPageChange = this.onPageChange.bind(this);
-    this.onTotalChannelChange = this.onTotalChannelChange.bind(this);
-    console.log("app constructor");
+    this.onTotalChannelChange = this.onTotalChannelChange.bind(this);    
+  }
+  resetQuery() {
+    this.urlParams = new URLSearchParams(window.location.search);
+    this.searchString = this.urlParams.get("search"); //default value of search input
+    this.tmpSearchString = this.searchString || "australia";
+    this.filterString = this.urlParams.get("filter") || "bycountry"; //default value of toggle buttons    
+    if((this.filterString !== "bycountry") && (this.tmpSearchString == "")) {
+      this.tmpSearchString = "90.7 fm"
+    }    
+    this.offset = ((this.urlParams.get("page")-1) > -1) ? ((this.urlParams.get("page")-1)*12) : 0 // for pagination, change by 12 every page, same as limit. page1 == offset0, page2==offset12
+    this.channels = [];
+    this.totalChannels = 0;
   }
 
   FreshSearch() { //execute brand new search based upon url params    
-    let urlParams = new URLSearchParams(window.location.search);
-    this.searchString = urlParams.get("search"); //default value of search input
-    let tmpSearchString = this.searchString || "australia";
-    this.filterString = urlParams.get("filter") || "bycountry"; //default value of toggle buttons    
-    if((this.filterString !== "bycountry") && (tmpSearchString == "")) {
-      tmpSearchString = "90.7 fm"
-    }    
-    this.offset = ((urlParams.get("page")-1) > -1) ? ((urlParams.get("page")-1)*12) : 0 // for pagination, change by 12 every page, same as limit. page1 == offset0, page2==offset12
-    this.channels = [];
-    this.totalChannels = 0;
+    
     // api_key = "AIzaSyAuW0tVBPyQQFkpXaB_2G7pcwViIB22DRg"; // old Youtube API
-    this.radio_query = "https://at1.api.radio-browser.info/json/stations/"; 
+    this.radio_query = this.selectedServer + "/json/stations/"; 
     //IMPORTANT: In future check for multiple servers response and use the one that's active, one was down last time
-    this.full_query = this.radio_query + this.filterString + "/" + tmpSearchString;
+    this.full_query = "https://" + this.radio_query + this.filterString + "/" + this.tmpSearchString;
     //fetch with cors
     console.log(this.full_query + "?limit=12" + "&offset=" + this.offset);
     fetch(this.full_query + "?limit=12" + "&offset=" + this.offset)
       .then(response => {
-        if (response.ok) {
+        if (response.ok) {          
           return response.json();
         }
         this.setState({
@@ -68,12 +97,48 @@ class App extends React.Component {
         );
       });       
   }
+  selectServer(index) {
 
+     var res = fetch("https://" + this.servers[index].name + "/json/stats")
+                .then(response => {                    
+                    if (response.ok) {
+                        this.selectedServer = this.servers[index].name;            
+                        this.FreshSearch();
+                        return 1;
+                    }                          
+                    else if((index+1) < this.servers.length) {
+                        console.info("trying next server");
+                        this.selectServer(index+1);
+                    }
+                    else {
+                      this.setState({
+                        error: true,
+                        loading: false
+                      });
+                      throw new Error('All servers are down');
+                    } 
+                }, 
+                error => {
+                  if((index+1) < this.servers.length) {
+                    console.info("trying next server");
+                    this.selectServer(index+1);
+                  }
+                  else {
+                    //set state error
+                    this.setState({
+                      error: true,
+                      loading: false
+                    });
+                    throw new Error('All servers are down');
+                  } 
+                }
+              );    
+    
+  }
 
 
   onTriggerSearch(value, event = undefined) { //when search icon is clicked or form is submitted with "enter"
-    //use this when search results need to update on re render    
-    console.log(event.currentTarget.tagName);
+    //use this when search results need to update on re render      
     if(!event.target.classList.value.includes("pagination")) { //not from pagination
     //for new searches, create new pagination and start from page 1
     //this line executes on toggle and form search click/enter
@@ -100,7 +165,7 @@ class App extends React.Component {
           loading: true
         })
       ); //this will trigger re render, it will remove the existing cards.
-      this.full_query = this.radio_query + this.filterString + "/" + valueLowerCase;
+      this.full_query =  "https://" + this.radio_query + this.filterString + "/" + valueLowerCase;
       fetch(this.full_query + "?limit=12" + "&offset=" + this.offset)
         .then(response => {
           if (response.ok) {
@@ -157,8 +222,8 @@ class App extends React.Component {
 
   //event for when back button is pressed
   componentDidMount() {
-    window.addEventListener("popstate", (e) => {
-      //set state
+    window.addEventListener("popstate", (e) => {      
+      this.resetQuery(); //grab new query params from url
       this.setState(
         (prevState, props) => ({
           loading: true
@@ -174,8 +239,7 @@ class App extends React.Component {
 
 
   render() {
-    console.log("app render"); 
-    console.log(this.offset);
+    console.log("app render");     
     return (
       <>
         <Header /> 
@@ -194,8 +258,12 @@ class App extends React.Component {
             />
           {
             this.state.error ? 
-            <div className="error-msg"> 
-              <h2>Error Occured</h2> 
+            <div className="cards columns is-multiline">                              
+              <div className="no-results column is-full p-0">
+                <div className="error-msg"> 
+                  <h2>Error, probably network problem or all radio api servers are down.</h2> 
+                </div>
+              </div>
             </div>
             :
             <Cards 
